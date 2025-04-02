f261ba79bf426e24d1bff7fdfacc8cef54136de6
Nithin Reddy <nkreddy@umich.edu>
2025-04-02 19:29:51 -0400

    Made right_turn.py into class based

--- algorithms/algorithm_32rr2m7i/src/right_turn.py
+++ algorithms/algorithm_32rr2m7i/src/right_turn.py
@@ -4,9 +4,15 @@ import numpy as np
 
 class right_turn:
     def __init__(self):
-        pass
+        self.image = None
+        self.hsv_image = None
+        self.white_mask = None
+        self.yellow_mask = None
+        self.mask = None
+        self.resized_mask = None
+        self.final = None
 
-    def update_mask(image, hsv_image):
+    def update_mask(self):
         #defining the ranges for HSV values
         yellow_lower_bound = np.array([12, 95, 0])
         yellow_upper_bound = np.array([32, 255, 255])
@@ -14,34 +20,33 @@ class right_turn:
         white_upper_bound = np.array([179, 103, 255])
         
         
-        white_mask = cv2.inRange(hsv_image, white_lower_bound, white_upper_bound)
-        yellow_mask = cv2.inRange(hsv_image, yellow_lower_bound, yellow_upper_bound) # Return a mask of HSV values within the range we specified
+        self.white_mask = cv2.inRange(self.hsv_image, white_lower_bound, white_upper_bound)
+        self.yellow_mask = cv2.inRange(self.hsv_image, yellow_lower_bound, yellow_upper_bound) # Return a mask of HSV values within the range we specified
         
-        white_mask = cv2.resize(white_mask, (image.shape[1], image.shape[0]))
-        yellow_mask = cv2.resize(yellow_mask, (image.shape[1], image.shape[0]))
+        self.white_mask = cv2.resize(self.white_mask, (self.image.shape[1], self.image.shape[0]))
+        self.yellow_mask = cv2.resize(self.yellow_mask, (self.image.shape[1], self.image.shape[0]))
 
         #some post processing stuff to help it look smoother
-        white_mask = cv2.erode(white_mask, None, iterations=2)
-        white_mask = cv2.dilate(white_mask, None, iterations=2)
+        self.white_mask = cv2.erode(self.white_mask, None, iterations=2)
+        self.white_mask = cv2.dilate(self.white_mask, None, iterations=2)
 
-        yellow_mask = cv2.erode(yellow_mask, None, iterations=2)
-        yellow_mask = cv2.dilate(yellow_mask, None, iterations=2)
+        self.yellow_mask = cv2.erode(self.yellow_mask, None, iterations=2)
+        self.yellow_mask = cv2.dilate(self.yellow_mask, None, iterations=2)
         
-        mask = cv2.bitwise_or(yellow_mask, white_mask)
+        self.mask = cv2.bitwise_or(self.yellow_mask, self.white_mask)
         
-        find_left_most_lane(yellow_mask, mask)
+        self.find_left_most_lane()
         
         # result = cv2.bitwise_and(image, image, mask=mask) #applies the mask to the base image so only masked parts of the image are shown
-        resized_mask = cv2.resize(mask, (640, 480))
+        self.resized_mask = cv2.resize(self.mask, (640, 480))
         # cv2.imshow("result", result)
-        cv2.imshow("mask", resized_mask)
+        cv2.imshow("mask", self.resized_mask)
         
         
-        
-    def find_left_most_lane(mask, final):
-        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    def find_left_most_lane(self):
+        contours, _ = cv2.findContours(self.mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         min_area = 200 # Adjust based on noise size
-        hsv_lanes = np.zeros_like(mask) #new occupancy grid that is blank
+        hsv_lanes = np.zeros_like(self.mask) #new occupancy grid that is blank
         
         min_x = float("inf")
         best_cnt = None
@@ -59,24 +64,24 @@ class right_turn:
                     best_cnt = (min_x, max_y)
                 
                 cv2.drawContours(hsv_lanes, [cnt], -1, 255, thickness=cv2.FILLED)
-                cv2.circle(final, best_cnt, 50, 255, -1)
+                cv2.circle(self.final, best_cnt, 50, 255, -1)
                 
-                cv2.line(final, (0, best_cnt[1]), (0, mask.shape[0]), 255, 10)
-                cv2.line(final, (best_cnt[0], best_cnt[1]), (0, best_cnt[1]), 255, 10)
+                cv2.line(self.final, (0, best_cnt[1]), (0, self.mask.shape[0]), 255, 10)
+                cv2.line(self.final, (best_cnt[0], best_cnt[1]), (0, best_cnt[1]), 255, 10)
         
     # Make main function that gets the image path and passes it into update mask as an HSV converted image
     # use cv2.imread(filename) then put the output into cv2.cvtcolor(image, cv2.RBGTOHSV)
 
     def run(self):
-        cap = cv2.VideoCapture("data/right_turn.mp4")
+        cap = cv2.VideoCapture(0)
         while cap.isOpened():
-            ret, image = cap.read()
+            ret, self.image = cap.read()
             if ret:
-                height, width, _ = image.shape
-                right_half_vid = image[:, width // 2:]
-                hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+                height, width, _ = self.image.shape
+                right_half_vid = self.image[:, width // 2:]
+                self.hsv_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
             
-                update_mask(image, hsv_image)
+                self.update_mask()
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     cap.release()
                     cv2.destroyAllWindows()
