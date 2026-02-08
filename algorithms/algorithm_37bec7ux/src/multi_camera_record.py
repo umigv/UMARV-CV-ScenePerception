a637fc83683c23f41f269293c85ae75146b32289
the2nake <41810559+the2nake@users.noreply.github.com>
2026-02-08 16:40:27 -0500

    feat(hdf5): add multi camera recording

---
+++ algorithms/algorithm_37bec7ux/src/multi_camera_record.py
@@ -0,0 +1,193 @@
+########################################################################
+#
+# Copyright (c) 2020, STEREOLABS.
+#
+# All rights reserved.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+########################################################################
+
+"""
+    Multi cameras sample showing how to open multiple ZED in one program
+"""
+
+import pyzed.sl as sl
+import h5py
+import cv2
+import numpy as np
+import threading
+import time
+import signal
+
+zed_list = []
+left_list = []
+depth_list = []
+timestamp_list = []
+thread_list = []
+stop_signal = False
+
+resolution_list = []
+
+timestamps = {}
+images = {}
+depths = {}
+
+
+def signal_handler(signal, frame):
+    global stop_signal
+    stop_signal = True
+    time.sleep(0.5)
+    exit()
+
+
+def grab_run(index):
+    global stop_signal
+    global zed_list
+    global timestamp_list
+    global left_list
+    global depth_list
+
+    runtime = sl.RuntimeParameters()
+    while not stop_signal:
+        err = zed_list[index].grab(runtime)
+        if err == sl.ERROR_CODE.SUCCESS:
+            zed_list[index].retrieve_image(left_list[index], sl.VIEW.LEFT, sl.MEM.CPU, resolution_list[index])
+            zed_list[index].retrieve_measure(
+                depth_list[index], sl.MEASURE.DEPTH, sl.MEM.CPU, resolution_list[index])
+            timestamp_list[index] = zed_list[index].get_timestamp(
+                sl.TIME_REFERENCE.CURRENT).data_ns
+        time.sleep(0.001)  # 1ms
+    zed_list[index].close()
+
+
+def main():
+    global stop_signal
+    global zed_list
+    global left_list
+    global depth_list
+    global timestamp_list
+    global thread_list
+    global resolution_list
+    global resolution_d_list
+    signal.signal(signal.SIGINT, signal_handler)
+
+    print("Running...")
+    init = sl.InitParameters()
+    init.camera_resolution = sl.RESOLUTION.HD720
+    init.camera_fps = 30  # The framerate is lowered to avoid any USB3 bandwidth issues
+
+    # List and open cameras
+    name_list = []
+    last_ts_list = []
+    cameras = sl.Camera.get_device_list()
+    index = 0
+    for cam in cameras:
+        init.set_from_serial_number(cam.serial_number)
+        name_list.append("{}".format(cam.serial_number))
+        print("Opening {}".format(name_list[index]))
+        zed_list.append(sl.Camera())
+        left_list.append(sl.Mat())
+        depth_list.append(sl.Mat())
+        timestamp_list.append(0)
+        last_ts_list.append(0)
+        status = zed_list[index].open(init)
+        if status != sl.ERROR_CODE.SUCCESS:
+            print(repr(status))
+            zed_list[index].close()
+        index = index + 1
+
+        resolution = cam.get_camera_information().camera_configuration.resolution
+        resolution_list.append(sl.Resolution(
+            min(720, resolution.width), min(404, resolution.height)))
+        
+        timestamps[cam.serial_number] = []
+        images[cam.serial_number] = []
+        depths[cam.serial_number] = []
+
+    # Start camera threads
+    for index in range(0, len(zed_list)):
+        if zed_list[index].is_opened():
+            thread_list.append(threading.Thread(
+                target=grab_run, args=(index,)))
+            thread_list[index].start()
+
+    # Display camera images
+    key = ''
+    while key != 113:  # for 'q' key
+        for index in range(0, len(zed_list)):
+            if zed_list[index].is_opened():
+                if (timestamp_list[index] > last_ts_list[index]):
+                    serial = name_list[index]
+                    timestamps[serial].append(timestamp_list[index])
+                    images[serial].append(left_list[index].get_data()[:, :, :3])
+                    depths[serial].append(depth_list[index].get_data())
+                    
+                    cv2.imshow(name_list[index], left_list[index].get_data())
+
+                    # get depth at central pixel
+                    x = round(depth_list[index].get_width() / 2)
+                    y = round(depth_list[index].get_height() / 2)
+                    err, depth_value = depth_list[index].get_value(x, y)
+                    if np.isfinite(depth_value):
+                        print("{} depth at center: {}MM".format(
+                            name_list[index], round(depth_value)))
+
+                    last_ts_list[index] = timestamp_list[index]
+        key = cv2.waitKey(10)
+    
+    hdf5_path = "out/multi_camera_record.hdf5"
+    print(f"\nWriting to {hdf5_path}...")
+
+
+    hf = h5py.File(hdf5_path, "w")
+    for i, cam in enumerate(cameras):
+        cam_info = cam.get_camera_information()
+        calibration_params = cam_info.camera_configuration.calibration_parameters
+
+        # LEFT CAMERA intrinsics
+        fx_left = calibration_params.left_cam.fx
+        fy_left = calibration_params.left_cam.fy
+        cx_left = calibration_params.left_cam.cx
+        cy_left = calibration_params.left_cam.cy
+
+        # RIGHT CAMERA intrinsics
+        fx_right = calibration_params.right_cam.fx
+        fy_right = calibration_params.right_cam.fy
+        cx_right = calibration_params.right_cam.cx
+        cy_right = calibration_params.right_cam.cy
+
+        tx = calibration_params.stereo_transform.get_translation().get()[0]
+
+        hf.create_dataset(f"inf{i}", data={"serial": cam.serial_number,
+                                           "fx_left": fx_left, "fy_left": fy_left, "cx_left": cx_left, "cy_left": cy_left, 
+                                           "fx_right": fx_right, "fy_right": fy_right, "cx_right": cx_right, "cy_right": cy_right, "tx": tx})
+        hf.create_dataset(f"tim{i}", data=timestamps[cam.serial_number])
+        hf.create_dataset(
+            f"img{i}", data=images[cam.serial_number], dtype="uint8", compression='gzip', compress_opts=9)
+        hf.create_dataset(
+            f"dep{i}", data=depths[cam.serial_number], compression='gzip', compress_opts=9)
+    hf.close()
+
+    cv2.destroyAllWindows()
+
+    # Stop the threads
+    stop_signal = True
+    for index in range(0, len(thread_list)):
+        thread_list[index].join()
+    
+    print("\nFINISH")
+
+
+if __name__ == "__main__":
+    main()
