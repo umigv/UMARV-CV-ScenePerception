8960d7de4684190d6e6dacaa40618f3ba9418eb8
Aditya Suri <aditya.as.suri@gmail.com>
2026-02-09 19:06:56 -0500

    Get better corner detection working

--- algorithms/algorithm_67dfefnm/src/no_mans_land.py
+++ algorithms/algorithm_67dfefnm/src/no_mans_land.py
@@ -41,67 +41,59 @@ class NoMansLand:
             if cv2.contourArea(cnt) > min_area:
                 return True, cnt
             
+        return False, []
+            
     def magntitude_of_scalar_projection(self, of: tuple[float, float], onto: tuple[float, float]):
         onto_mag = ((onto[0] ** 2) + (onto[1] ** 2)) ** 0.5
         of_dot_onto = of[0] * onto[0] + of[1] * onto[1]
 
-        return of_dot_onto / onto_mag
+        return abs(of_dot_onto / onto_mag)
 
     def grab_ramp_corners(self, best_cnt):
-        # min_x = self.width - 1
-        # max_x = 0
-        # min_y = self.height - 1
-        # max_y = 0
-
-        # for point in best_cnt:
-        #     if point[0, 0] > max_x:
-        #         max_x = point[0, 0]
-        #     if point[0, 0] < min_x:
-        #         min_x = point[0, 0]
-
-        #     if point[0, 1] > max_y:
-        #         max_y = point[0, 1]
-        #     if point[0, 1] < min_y:
-        #         min_y = point[0, 1]
-
-        # # cv2.circle(self.final, (min_x, max_y), 5, 128, -1)
-        # # cv2.circle(self.final, (max_x, max_y), 5, 128, -1)
-
-        # return (min_x, max_y), (max_x, max_y), (min_x, min_y), (max_x, min_y) #bottom left, bottom right, top left, top right
-
         diag_length = ((self.width ** 2) + (self.height ** 2)) ** 0.5
 
-        # reference vectors have (0, 0) at bottom_left, with x, y increasing rightwards, upwards respectively
-        bl_ref_vec = (1, -1)
+        bl_ref_vec = (1.0, -1.0)
         most_bl_point = (best_cnt[0, 0, 0], best_cnt[0, 0, 1])
         min_bl_dist = diag_length
 
-        br_ref_vec = (-1, -1)
+        br_ref_vec = (-1.0, -1.0)
         most_br_point = (best_cnt[0, 0, 0], best_cnt[0, 0, 1])
         min_br_dist = diag_length
 
-        tl_ref_vec = (1, 1)
+        tl_ref_vec = (1.0, 2.0)
         most_tl_point = (best_cnt[0, 0, 0], best_cnt[0, 0, 1])
         min_tl_dist = diag_length
 
-        tr_ref_vec = (-1, 1)
+        tr_ref_vec = (-1.0, 2.0)
         most_tr_point = (best_cnt[0, 0, 0], best_cnt[0, 0, 1])
         min_tr_dist = diag_length
 
         for point in best_cnt:
-            x, y = point[0, 0], point[0, 1]
+            x, y = float(point[0, 0]), float(point[0, 1])
 
-            bl_diff_vec = (x, self.height - 1 - y)
+            bl_diff_vec = (x, -1 * (self.height - 1 - y))
             bl_dist = self.magntitude_of_scalar_projection(bl_diff_vec, bl_ref_vec)
             if bl_dist < min_bl_dist:
                 min_bl_dist = bl_dist
-                most_bl_point = (x, y)
+                most_bl_point = (int(x), int(y))
 
-            br_diff_vec = (self.width - 1 - x, self.height - 1 - y)
+            br_diff_vec = (-1 * (self.width - 1 - x), -1 * (self.height - 1 - y))
             br_dist = self.magntitude_of_scalar_projection(br_diff_vec, br_ref_vec)
-            if br_dist < min_bl_dist:
-                min_br_dist = bl_dist
-                most_br_point = (x, y)
+            if br_dist < min_br_dist:
+                min_br_dist = br_dist
+                most_br_point = (int(x), int(y))
+
+            tl_diff_vec = (x, y)
+            tl_dist = self.magntitude_of_scalar_projection(tl_diff_vec, tl_ref_vec)
+            if tl_dist < min_tl_dist:
+                min_tl_dist = tl_dist
+                most_tl_point = (int(x), int(y))
+
+            tr_diff_vec = (-1 * (self.width - 1 - x), y)
+            tr_dist = self.magntitude_of_scalar_projection(tr_diff_vec, tr_ref_vec)
+            if tr_dist < min_tr_dist:
+                min_tr_dist = tr_dist
+                most_tr_point = (int(x), int(y))
 
         return most_bl_point, most_br_point, most_tl_point, most_tr_point
             
@@ -111,6 +103,25 @@ class NoMansLand:
 
     def state_2(self, ramp_cnt): # on ramp
         bottom_left, bottom_right, top_left, top_right = self.grab_ramp_corners(ramp_cnt)
+
+        cv2.line(self.final, bottom_left, (0, self.height), 255, 10)
+        cv2.line(self.final, bottom_right, (self.width, self.height), 255, 10)
+
+        mid_bottom = ((bottom_left[0] + bottom_right[0]) // 2, (bottom_left[1] + bottom_right[1]) // 2)
+        mid_top = ((top_left[0] + top_right[0]) // 2, (top_left[1] + top_right[1]) // 2)
+        cv2.circle(self.final, mid_bottom, 10, 128, -1)
+        cv2.circle(self.final, mid_top, 10, 128, -1)
+
+        cv2.circle(self.final, bottom_left, 10, 128, -1)
+        cv2.circle(self.final, bottom_right, 10, 128, -1)
+        cv2.circle(self.final, top_left, 10, 128, -1)
+        cv2.circle(self.final, top_right, 10, 128, -1)
+
+        print(bottom_left)
+        print(bottom_right)
+        print(top_left)
+        print(top_right)
+        print()
         
     def run(self):
         cap = cv2.VideoCapture('data/ramp.MOV') # 0 for webcam # 1,2 for external cameras
@@ -124,34 +135,13 @@ class NoMansLand:
 
             if ret:
                 self.update_mask()
-                # cnts, _ = cv2.findContours(self.ramp_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-                # self.ramp_mode = False
-
-                # bottom_left = (None, None)
-                # bottom_right = (None, None)
-                # top_left = (None, None)
-                # top_right = (None, None)
-
-                # min_area = 1000
-                # for cnt in cnts:
-                #     if cv2.contourArea(cnt) > min_area:
-                #         self.ramp_mode = True
-                #         bottom_left, bottom_right, top_left, top_right = self.grab_ramp_corners(cnt)
-                        
-                #         cv2.line(self.final, bottom_left, (0, self.height), 255, 10)
-                #         cv2.line(self.final, bottom_right, (self.width, self.height), 255, 10)
-
-                #         mid_bottom = ((bottom_left[0] + bottom_right[0]) // 2, (bottom_left[1] + bottom_right[1]) // 2)
-                #         mid_top = ((top_left[0] + top_right[0])// 2, (top_left[1] + top_right[1]) // 2)
-                #         cv2.circle(self.final, mid_bottom, 5, 128, -1) #radius, color, ?
-                #         cv2.circle(self.final, mid_top, 5, 128, -1)
-
+                ramp_visible = False
                 ramp_visible, best_cnt = self.is_ramp_visible()
 
                 if ramp_visible:
                     print("ramp mode")
-                    self.state2()
+                    self.state_2(best_cnt)
                 else:
                     self.state_1()
 
@@ -185,7 +175,7 @@ class NoMansLand:
 def main():
     obj = NoMansLand()
     obj.run()
-    obj.detect_ramp(123) #fill with color range (green)
+    # obj.detect_ramp(123) #fill with color range (green)
 
 if __name__ == "__main__":
     main()
